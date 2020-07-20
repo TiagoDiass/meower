9276cf8b755d501cc01aa634aa66f30c039ae6ed
TiagoDiass <tiago.costadiasss@gmail.com>
2020-07-20 14:29:56 -0300

    Header component done

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,9 +1,40 @@
 <template>
-  <div id="app"></div>
+  <div id="app">
+    <Header />
+  </div>
 </template>
 
 <script>
+import Header from './components/Header';
+
 export default {
   name: 'App',
+  components: {
+    Header,
+  },
 };
 </script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300;400;600;700&display=swap');
+
+* {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
+
+html,
+body {
+  height: 100%;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  background-color: #fdfdfd;
+}
+
+#app {
+  font-family: 'Source Sans Pro', sans-serif;
+  width: 80%;
+}
+</style>
---
+++ frontend/src/components/Header.vue
@@ -0,0 +1,20 @@
+<template>
+  <header class="header">
+    <h1>Meower · The Twitter for Cats 😸</h1>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'header',
+};
+</script>
+
+<style>
+.header h1 {
+  font-weight: 400;
+  font-size: 3rem;
+  display: flex;
+  justify-content: center;
+}
+</style>
